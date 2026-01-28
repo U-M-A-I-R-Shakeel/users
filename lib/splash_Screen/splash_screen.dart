ac8e714bf6ce2acf769ad05211dae3f7dae8f6f6
U-M-A-I-R-Shakeel <umairhashmi363@gmail.com>
2026-01-28 23:20:33 +0500

    Splash/Login/Register

--- lib/splash_Screen/splash_screen.dart
+++ lib/splash_Screen/splash_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:users/Assistants/assistant_methods.dart';
+import 'package:users/global/global.dart';
+import 'package:users/screens/login_screen.dart';
+import 'package:users/screens/main_page.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -8,12 +13,38 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  startTimer(){
+    Timer(Duration(seconds: 3), () async {
+      if(await firebaseAuth.currentUser !=null) {
+        firebaseAuth.currentUser != null ? AssistantMethods
+            .readCurrentOnLineUserInfo() : null;
+        Navigator.push(
+            context, MaterialPageRoute(builder: (c) => MainScreen()));
+      }
+      else{
+        Navigator.push(context, MaterialPageRoute(builder: (c) => LoginScreen()));
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    startTimer();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Text(
           'Ride Sharing',
+          style: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.bold
+          ),
         ),
       ),
     ) ;
